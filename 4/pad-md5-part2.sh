0e827259c9aa29c4077462a13165ff9dd2a25cf3
Don <donabrams@gmail.com>
2015-12-14 08:12:47 -0700

    day 4 pt 2 complete

---
+++ 4/pad-md5-part2.sh
@@ -0,0 +1,11 @@
+#!usr/bin/env bash
+
+PASSWORD=$(cat password.txt)
+
+while [[ ${MD5:0:6} != "000000" ]]; do
+	NUM=$[$NUM + 1]
+	MD5=$(printf "%s%d" ${PASSWORD} ${NUM} | md5)
+#	echo ${NUM} ${MD5} ${MD5:0:5}
+done
+
+echo ${NUM}
